2b06f79e7396f418a1dbdc87d4cb77d8106581a0
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2021-12-02 03:36:23 +0530

    feat: Sphinx documentation

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-from .test_app import test_create_form
+from .test_forms import test_create_form
 from .test_actions import test_register_action
 from .test_questions import test_create_question
 from .test_answers import test_create_answer
